f8e9597f423f96cab07d24c77e854d39574b9012
blubrom <clement.morand@ens-rennes.fr>
2022-10-12 20:34:25 +0200

    mise à jour du script pour refléter les expériences de l'article

--- EGC2023/CRI/experiments.sh
+++ EGC2023/CRI/experiments.sh
@@ -2,7 +2,13 @@
 
 output_file="results/experiments_results.md"
 
-echo "première expérience TAXREF (avec abbréviations)"
+
+echo "expérience LINNAEUS"
+echo "# expérience LINNAEUS" >> $output_file
+python3 Experiences/score_corpus.py -o $output_file -cl LINNAEUS
+
+
+echo "Table 1"
 echo "# première expérience TAXREF (avec abbréviations)" >> $output_file
 python3 Experiences/score_corpus.py -o $output_file -cl TAXREF  -m A -s Experiences/empty.txt
 
@@ -11,17 +17,13 @@ echo "# première expérience TAXREF (sans abbréviations)" >> $output_file
 python3 Experiences/score_corpus.py -o $output_file -cl TAXREF -m raw -s Experiences/empty.txt
 
 
-echo "expérience LINNAEUS"
-echo "# expérience LINNAEUS" >> $output_file
-python3 Experiences/score_corpus.py -o $output_file -cl LINNAEUS
-
-echo "expérience Table 1 (results in analyses/sondage29juilletXtaxref.txt)"
+echo "expérience Table 2 (results in analyses/sondage29juilletXtaxref.txt)"
 python3 Experiences/taxref-modulo-distance
 
 
-echo "expériences section 4.2"
-echo "# expériences section 4.2" >> $output_file
-for classifier in "ABS7" "ABS5" "ABS3" 
+echo "expériences Table 3"
+echo "# expériences Table 3" >> $output_file
+for classifier in "ABS7" "ABS5" "ABS4" "ABS3" "ABS2" 
 do
     echo $classifier
     python3 Experiences/score_corpus.py -o $output_file -cl $classifier -s Experiences/empty.txt
@@ -49,10 +51,8 @@ echo "# expériences figure 1 TAXREF rang 3" >> $output_file
 python3 Experiences/decision_bound_procedure.py -m TAXREF3 -o $output_file
 echo "you can find the csv for the experiments in the analyses folder at evolution_latin.csv and evolution_TAXREF.csv" 
 
-echo "expériences table 2"
-echo "# expériences table 2" >> $output_file
-(time python3 Experiences/score_corpus.py -o $output_file -cl TAXREF -m raw -vs test)
-(time python3 Experiences/score_corpus.py -o $output_file -cl TAXREF -m A -vs test)
+echo "expériences table 4"
+echo "# expériences table 4" >> $output_file
 for classifier in 'ABS3' 'LATIN'
 do
     for mode in 'A' 'A:MM' 'A:mm:MM' 
